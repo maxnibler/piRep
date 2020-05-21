bfc330a3406d9f805a536b74f3359f743d9875b3
Max <max.nibler@gmail.com>
2020-05-21 12:27:43 -0700

    switching to system() for base functionality

---
+++ Stocks/Cpy/main.cpp
@@ -0,0 +1,17 @@
+//Maximillian H Nibler
+//max.nibler@gmail.com
+//https://github.com/maxnibler/
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+
+using namespace std;
+
+int main (/*int argc, char* argv[]*/){
+  system("python test.py");
+  return 0;
+}
