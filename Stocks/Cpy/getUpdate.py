7abf8072ec7b4a2ceda8a26c6aff843b2ccf468d
Max <max.nibler@gmail.com>
2020-05-24 23:54:01 -0700

    add getUpdate script

---
+++ Stocks/Cpy/getUpdate.py
@@ -0,0 +1,26 @@
+ #Maximillian H Nibler
+#max.nibler@gmail.com
+#https://github.com/maxnibler/
+
+import sys
+import yfinance as yf
+
+argLen = len(sys.argv)
+
+if argLen != 2:
+    exit()
+
+name = sys.argv[1]
+stockTicker = yf.Ticker(name)
+#print("ticker set")
+update = stockTicker.history(period="1m",interval="1m")
+#print("api call made")
+path = "Database/"
+path = path+name+"/"+name+"_update.txt"
+
+outPath = open(path,'w')
+
+outPath.write(update.to_string())
+outPath.write("\n")
+outPath.close()
+#print("python out")
